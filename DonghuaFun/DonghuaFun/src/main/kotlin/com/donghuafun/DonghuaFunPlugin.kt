65a0f9c08e2e772d15d77e6233956e50e060e365
AC321-beep <charanac4@gmail.com>
2026-06-06 13:39:11 +0530

    Create DonghuaFunPlugin.kt

---
+++ DonghuaFun/DonghuaFun/src/main/kotlin/com/donghuafun/DonghuaFunPlugin.kt
@@ -0,0 +1,12 @@
+package com.donghuafun
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DonghuaFunPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(DonghuaFun())
+    }
+}
