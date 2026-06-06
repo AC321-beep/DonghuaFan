d64e365b97658df9d547a0181f2f733820ef544a
AC321-beep <charanac4@gmail.com>
2026-06-06 13:37:53 +0530

    Create build.gradle.kts

---
+++ DonghuaFun/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "en"
+    description = "Watch Donghua from DonghuaFun with English episode guides."
+    authors = listOf("AC321-beep")
+    status = 1
+    tvTypes = listOf("Anime")
+    iconUrl = "https://donghuafun.com/template/shoutu45/assets/images/logo-1.png"
+}
