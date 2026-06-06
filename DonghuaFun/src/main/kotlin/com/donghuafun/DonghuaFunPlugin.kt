d1cc37090d3fa5cbf513e50e54dcd0cd1e65d25e
AC321-beep <charanac4@gmail.com>
2026-06-06 23:09:47 +0530

    Update DonghuaFunPlugin.kt

--- DonghuaFun/src/main/kotlin/com/donghuafun/DonghuaFunPlugin.kt
+++ DonghuaFun/src/main/kotlin/com/donghuafun/DonghuaFunPlugin.kt
@@ -1,13 +1,12 @@
 package com.donghuafun
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class DonghuaFunPlugin: Plugin() {
+class DonghuaFunPlugin : Plugin() {
     override fun load(context: Context) {
-        // Registers your main scraping provider class
         registerMainAPI(DonghuaFunProvider())
     }
 }
