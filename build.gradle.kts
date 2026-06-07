1df8c07c54648341fade0b690cfad38c2c12bd38
AC321-beep <charanac4@gmail.com>
2026-06-07 11:28:13 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,9 @@ buildscript {
         mavenCentral()
         maven("https://jitpack.io")
     }
-
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        // FIX: Points explicitly to the master-SNAPSHOT compilation branch
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:93ede36a08")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
@@ -27,7 +25,6 @@ allprojects {
 }
 
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
 fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
 
 subprojects {
@@ -41,18 +38,15 @@ subprojects {
 
     android {
         namespace = "com.example"
-
         defaultConfig {
             minSdk = 21
             compileSdkVersion(35)
             targetSdk = 35
         }
-
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
         }
-
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
                 jvmTarget.set(JvmTarget.JVM_1_8)
@@ -70,7 +64,6 @@ subprojects {
         val implementation by configurations
 
         cloudstream("com.lagradost:cloudstream3:pre-release")
-
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
         implementation("org.jsoup:jsoup:1.18.3")
