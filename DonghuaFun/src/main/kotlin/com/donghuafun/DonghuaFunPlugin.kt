47a8d06fd2161147b39e687c1fef495fcb32840e
AC321-beep <charanac4@gmail.com>
2026-06-07 12:15:48 +0530

    Update DonghuaFunPlugin.kt

--- DonghuaFun/src/main/kotlin/com/donghuafun/DonghuaFunPlugin.kt
+++ DonghuaFun/src/main/kotlin/com/donghuafun/DonghuaFunPlugin.kt
@@ -1,12 +1,13 @@
-package com.donghuafun
-
-import android.content.Context
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-
 @CloudstreamPlugin
 class DonghuaFunPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(DonghuaFunProvider())
+        registerExtractorAPI(Vtbe())
+        registerExtractorAPI(wishfast())
+        registerExtractorAPI(waaw())
+        registerExtractorAPI(FileMoonSx())
+        registerExtractorAPI(Ultrahd())
+        registerExtractorAPI(Rumble())
+        registerExtractorAPI(PlayStreamplay())
     }
 }
