469dd86c86fde0e11a025bf8a47118c9c3f434e7
AC321-beep <charanac4@gmail.com>
2026-06-10 00:46:18 +0530

    Update build.gradle.kts

--- MyAnimeLive/build.gradle.kts
+++ MyAnimeLive/build.gradle.kts
@@ -1,1 +1,12 @@
+android {
+    namespace = "com.myanimelive"
+}
 
+cloudstream {
+    description = "Watch anime from myanime.live (Dailymotion)"
+    authors = listOf("AC321-beep")
+    status = 1
+    tvTypes = listOf("Anime")
+    language = "en"
+    // iconUrl = optional
+}
