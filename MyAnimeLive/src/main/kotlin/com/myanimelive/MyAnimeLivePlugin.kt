2a4917b89b4415afc4f590626b779b881f25cc13
AC321-beep <charanac4@gmail.com>
2026-06-10 00:11:22 +0530

    Create MyAnimeLivePlugin.kt

---
+++ MyAnimeLive/src/main/kotlin/com/myanimelive/MyAnimeLivePlugin.kt
@@ -0,0 +1,14 @@
+package com.myanimelive
+
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.Dailymotion
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class MyAnimeLivePlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(MyAnimeLiveProvider())
+        registerExtractorAPI(Dailymotion())
+    }
+}
