96f0975d009f5064e704f69799ffbdf78bacbba0
AC321-beep <charanac4@gmail.com>
2026-06-10 10:37:00 +0530

    Update build.gradle.kts

--- MyAnimeLive/build.gradle.kts
+++ MyAnimeLive/build.gradle.kts
@@ -5,8 +5,9 @@ android {
 cloudstream {
     description = "Anime from myanimelive"
     authors = listOf("AC321-beep")
-    status = 1
+    status = 1                // 0 = disabled, 1 = working, 2 = broken, etc.
     tvTypes = listOf("Anime")
     language = "en"
+    version = 1               // ⬅️ Add this line – must be an integer
     // iconUrl = optional
 }
