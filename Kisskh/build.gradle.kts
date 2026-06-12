92adf7ebda3cc750a2feeffd71a7f500b4b911c8
AC321-beep <charanac4@gmail.com>
2026-06-13 02:48:45 +0530

    Update build.gradle.kts

--- Kisskh/build.gradle.kts
+++ Kisskh/build.gradle.kts
@@ -1,18 +1,10 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-}
-
 android {
-    compileSdk = 33
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 33
-    }
+    namespace = "com.kisskh"
 }
 
 dependencies {
-    compileOnly("com.github.recloudstream:cloudstream:pre-release")
-    implementation("org.jsoup:jsoup:1.15.3")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.4")
+    // Force the correct dependency for this module only
+    implementation("com.github.recloudstream:cloudstream:master-SNAPSHOT") {
+        isTransitive = false
+    }
 }
