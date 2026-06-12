3951a39ad74a3329336135b0f3a867ecabf80e92
AC321-beep <charanac4@gmail.com>
2026-06-12 14:24:30 +0530

    Update DonghuaFunProvider.kt

--- DonghuaFun/src/main/kotlin/com/donghuafun/DonghuaFunProvider.kt
+++ DonghuaFun/src/main/kotlin/com/donghuafun/DonghuaFunProvider.kt
@@ -147,7 +147,7 @@ class DonghuaFunProvider : MainAPI() {
         return match?.groupValues?.get(1)?.toIntOrNull() ?: -1
     }
 
-   @Suppress("DEPRECATION", "DEPRECATION_ERROR")
+    @Suppress("DEPRECATION", "DEPRECATION_ERROR")
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
@@ -180,49 +180,7 @@ class DonghuaFunProvider : MainAPI() {
                         url = rawUrl,
                         referer = data,
                         quality = Qualities.Unknown.value,
-                        type = INFER_TYPE
-                    )
-                )
-                return true
-            }
-        }
-
-        val doc = try { app.get(data, headers = headers).document } catch (e: Exception) { null }
-        doc?.select("iframe[src*='dailymotion']")?.forEach { iframe ->
-            val src = iframe.attr("src")
-            val dmId = extractDailymotionId(src)
-            if (dmId != null) {
-                val videoUrl = "https://www.dailymotion.com/video/$dmId"
-                return loadExtractor(videoUrl, data, subtitleCallback, callback)
-            }
-        }
-
-        val dmIdMatch = Regex("""dailymotion\.com/(?:embed/)?video/([a-zA-Z0-9]+)""").find(html)
-        if (dmIdMatch != null) {
-            val dmId = dmIdMatch.groupValues[1]
-            val videoUrl = "https://www.dailymotion.com/video/$dmId"
-            return loadExtractor(videoUrl, data, subtitleCallback, callback)
-        }
-
-        doc?.select("iframe[src]")?.forEach { iframe ->
-            val src = fixUrl(iframe.attr("src"))
-            if (src.isNotBlank() && !src.contains("dailymotion")) {
-                if (loadExtractor(src, data, subtitleCallback, callback)) return true
-            }
-        }
-
-        Log.d(TAG, "No playable source found for $data")
-        return false
-    }
-            else if (rawUrl != null && (rawUrl.contains(".m3u8", ignoreCase = true) || rawUrl.contains(".mp4", ignoreCase = true))) {
-                callback.invoke(
-                    ExtractorLink(
-                        source = this.name,
-                        name = this.name,
-                        url = rawUrl,
-                        referer = data,
-                        quality = Qualities.Unknown.value,
-                        type = INFER_TYPE
+                        type = if (rawUrl.contains(".m3u8", ignoreCase = true)) ExtractorLinkType.M3U8 else ExtractorLinkType.VIDEO
                     )
                 )
                 return true
