effaeb312cd895174a5576c661867226727aa401
AC321-beep <charanac4@gmail.com>
2026-06-13 22:34:04 +0530

    Update FileMoonExtractor.kt

--- DonghuaStream/src/main/kotlin/com/donghuastream/Extractors/FileMoonExtractor.kt
+++ DonghuaStream/src/main/kotlin/com/donghuastream/Extractors/FileMoonExtractor.kt
@@ -1,1 +1,8 @@
+package com.donghuastream.extractors
 
+import com.lagradost.cloudstream3.extractors.Filesim
+
+class FileMoonExtractor : Filesim() {
+    override val mainUrl = "https://filemoon.sx"
+    override val name = "FileMoonSx"
+}
