d5727cbc520f2809193d02aebfecc03bce3f1b84
AC321-beep <charanac4@gmail.com>
2026-06-13 09:16:44 +0530

    Update build.gradle.kts

--- Kisskh/build.gradle.kts
+++ Kisskh/build.gradle.kts
@@ -2,21 +2,11 @@ version = 1
 
 android {
     defaultConfig {
-        val localProperties = java.util.Properties()
-        val localFile = rootProject.file("local.properties")
-        if (localFile.exists()) {
-            localProperties.load(localFile.inputStream())
-        }
-        val kissKhApi = localProperties.getProperty("KissKh") ?: System.getenv("KissKh") ?: ""
-        val kisskhSubApi = localProperties.getProperty("KisskhSub") ?: System.getenv("KisskhSub") ?: ""
-
-        buildConfigField("String", "KissKh", "\"$kissKhApi\"")
-        buildConfigField("String", "KisskhSub", "\"$kisskhSubApi\"")
     }
 }
 
 cloudstream {
-    description = "Korean, Chinese, Philippine dramas and anime from Kisskh"
+    description = "Mostly Asian Content "
     authors = listOf(providers.gradleProperty("cs_author").orNull ?: "AC321-beep")
     status = 1
     tvTypes = listOf("AsianDrama", "Anime")
