6a798f1ca7c6df718adae661ad7c6202aefe980b
AC321-beep <charanac4@gmail.com>
2026-06-14 02:13:27 +0530

    Update build.gradle.kts

--- DonghuaStream/build.gradle.kts
+++ DonghuaStream/build.gradle.kts
@@ -4,6 +4,6 @@ cloudstream {
     status = 1
     tvTypes = listOf("Anime")
     language = "zh"
-    version = 2
+    version = 3
     iconUrl = "https://donghuastream.org/template/shoutu45/assets/images/logo-1.png"
 }
