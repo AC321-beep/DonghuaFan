c85a6cca22871ec54d653c199678e1fea3b3207f
AC321-beep <charanac4@gmail.com>
2026-06-13 22:03:10 +0530

    Update DonghuastreamPlugin.kt

--- DonghuaStream/src/main/kotlin/com/donghuastream/DonghuastreamPlugin.kt
+++ DonghuaStream/src/main/kotlin/com/donghuastream/DonghuastreamPlugin.kt
@@ -1,1 +1,11 @@
+package com.donghuastream
 
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DonghuastreamPlugin : Plugin() {
+    override fun load() {
+        registerMainAPI(DonghuastreamProvider())
+    }
+}
