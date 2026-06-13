9c27b9e42cc337674463c0a0cd530a960ca77c2b
AC321-beep <charanac4@gmail.com>
2026-06-13 22:34:32 +0530

    Update StreamSBExtractor.kt

--- DonghuaStream/src/main/kotlin/com/donghuastream/Extractors/StreamSBExtractor.kt
+++ DonghuaStream/src/main/kotlin/com/donghuastream/Extractors/StreamSBExtractor.kt
@@ -1,1 +1,7 @@
+package com.donghuastream.extractors
 
+import com.lagradost.cloudstream3.extractors.StreamSB
+
+class StreamSBExtractor : StreamSB() {
+    override var mainUrl = "https://waaw.to"
+}
