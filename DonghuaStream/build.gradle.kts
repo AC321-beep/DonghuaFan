afc62fa616392a9d8313859785567cd6f3312e08
AC321-beep <charanac4@gmail.com>
2026-06-14 01:05:37 +0530

    Create build.gradle.kts

---
+++ DonghuaStream/build.gradle.kts
@@ -0,0 +1,10 @@
+cloudstream {
+    name = "DonghuaStream"
+    description = "Donghua"
+    authors = listOf("AC321-beep")
+    status = 1
+    tvTypes = listOf("Anime")
+    language = "zh"
+    version = 2
+    iconUrl = "https://donghuastream.org/template/shoutu45/assets/images/logo-1.png"
+}
