fa3531e21276205476815a1fd3119e8c8fc05c3d
AC321-beep <charanac4@gmail.com>
2026-06-13 22:34:55 +0530

    Update StreamWishExtractor.kt

--- DonghuaStream/src/main/kotlin/com/donghuastream/Extractors/StreamWishExtractor.kt
+++ DonghuaStream/src/main/kotlin/com/donghuastream/Extractors/StreamWishExtractor.kt
@@ -1,1 +1,8 @@
+package com.donghuastream.extractors
 
+import com.lagradost.cloudstream3.extractors.StreamWishExtractor
+
+class StreamWishExtractor : StreamWishExtractor() {
+    override var mainUrl = "https://wishfast.top"
+    override var name = "StreamWish"
+}
