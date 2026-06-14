c31e33577871ec85fad887a5079a538c914f02be
AC321-beep <charanac4@gmail.com>
2026-06-14 17:36:18 +0530

    Update build.gradle.kts

--- DonghuaStream/build.gradle.kts
+++ DonghuaStream/build.gradle.kts
@@ -1,9 +1,9 @@
 cloudstream {
-    description = "Donghua"
+    description = "Donghua (Multi-Source)"
     authors = listOf("AC321-beep")
     status = 1
     tvTypes = listOf("Anime")
     language = "zh"
-    version = 9
+    version = 10
     iconUrl = "https://donghuastream.org/wp-content/uploads/2023/05/Donghua-Stream--1024x1024.jpeg"
 }
