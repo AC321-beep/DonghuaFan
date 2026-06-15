d6818ee78b845887bbf5b9fd519e91125c5d1040
AC321-beep <charanac4@gmail.com>
2026-06-15 14:03:54 +0530

    Update build.gradle.kts

--- LiveSports/build.gradle.kts
+++ LiveSports/build.gradle.kts
@@ -1,1 +1,26 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
 
+android {
+    namespace = "com.livesports"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+cloudstream {
+    description = "Live Sports Events and Matches"
+    authors = listOf("AC321-beep")
+    status = 1
+    tvTypes = listOf("Live")
+    language = "en"
+    version = 1
+}
