fa507388799094d755da75a91ee34e3185cfa73e
AC321-beep <charanac4@gmail.com>
2026-06-15 14:24:53 +0530

    Update LiveSportsPlugin.kt

--- LiveSports/src/main/kotlin/com/livesports/LiveSportsPlugin.kt
+++ LiveSports/src/main/kotlin/com/livesports/LiveSportsPlugin.kt
@@ -7,8 +7,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class LiveSportsPlugin: Plugin() {
     override fun load(context: Context) {
-        // Pass context to the provider so WebView can render headless browsers
-        LiveSportsProvider.context = context
         registerMainAPI(LiveSportsProvider())
     }
 }
