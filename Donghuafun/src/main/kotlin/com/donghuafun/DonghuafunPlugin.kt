8b73a824f98a810b7b117ccaffdfb3d5b098e1a4
AC321-beep <charanac4@gmail.com>
2026-06-16 21:24:47 +0530

    Update DonghuafunPlugin.kt

--- Donghuafun/src/main/kotlin/com/donghuafun/DonghuafunPlugin.kt
+++ Donghuafun/src/main/kotlin/com/donghuafun/DonghuafunPlugin.kt
@@ -1,14 +1,13 @@
 package com.donghuafun
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.extractors.Dailymotion   
-import android.content.Context
 
 @CloudstreamPlugin
 class DonghuafunPlugin : Plugin() {
     override fun load(context: Context) {
+        // Registers your updated 4K Donghua Scraper
         registerMainAPI(DonghuaFunProvider())
-        registerExtractorAPI(Dailymotion())  
     }
 }
