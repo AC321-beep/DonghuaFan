6de5118f95d248fa25d5435469ea0d21c8194fae
AC321-beep <charanac4@gmail.com>
2026-06-29 08:41:33 +0530

    Create build.gradle.kts

---
+++ FootballReplays/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    authors = listOf("AC321-beep")
+    descriptions = ("Watch Football Match Replays & Highlights")
+    language = "en"
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Sports") 
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.footreplays.com/&size=128"
+}
