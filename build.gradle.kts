91831ad2f22bdf2699a873feccff1d89a15e9b5a
AC321-beep <charanac4@gmail.com>
2026-06-29 20:27:10 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ allprojects {
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
 
+// ... (keep the top of the file exactly as you provided it) ...
+
 subprojects {
     apply(plugin = "com.android.library")
     apply(plugin = "kotlin-android")
@@ -63,8 +65,13 @@ subprojects {
         val cloudstream by configurations
         val implementation by configurations
 
+        // This is the official Cloudstream library
         cloudstream("com.lagradost:cloudstream3:pre-release")
         implementation(kotlin("stdlib"))
+        
+        // 🟢 ADDED: This fixes the "Unresolved reference" for coroutines (async/await)
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") 
+        
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
         implementation("org.jsoup:jsoup:1.18.3")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
