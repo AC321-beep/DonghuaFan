96d4e741110f418e21770210f3e4bce5f9986b07
AC321-beep <charanac4@gmail.com>
2026-06-29 15:38:23 +0530

    Update build.gradle.kts

--- LuciferDonghua/build.gradle.kts
+++ LuciferDonghua/build.gradle.kts
@@ -1,9 +1,14 @@
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
+
 cloudstream {
-    description = "Watch Donghua from Multi-Source"
-    authors = listOf("AC321-beep")
-    status = 1
-    tvTypes = listOf("Anime")
-    language = "zh"
-    version = 2
-    iconUrl = "https://i0.wp.com/luciferdonghua.in/wp-content/uploads/2022/12/cropped-lucifer-donghua-DP-192x192.webp"
+    setParams {
+        description = "Watch Donghua from Multi-Source"
+        authors = listOf("AC321-beep")
+        tvTypes = listOf(TvType.Anime) // Must use TvType enum
+        language = "zh"
+        version = 3
+        iconUrl = "https://i0.wp.com/luciferdonghua.in/wp-content/uploads/2022/12/cropped-lucifer-donghua-DP-192x192.webp"
+    }
 }
