30ca2e8e878cb14dc84c05670abd4aaa51762049
AC321-beep <charanac4@gmail.com>
2026-06-29 15:43:35 +0530

    Update build.gradle.kts

--- LuciferDonghua/build.gradle.kts
+++ LuciferDonghua/build.gradle.kts
@@ -1,14 +1,15 @@
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+cloudstream {
+    description = "Watch Donghua from Multi-Source"
+    authors = listOf("AC321-beep")
+    status = 1
+    tvTypes = listOf("Anime")
+    language = "zh"
+    version = 2
+    iconUrl = "https://i0.wp.com/luciferdonghua.in/wp-content/uploads/2022/12/cropped-lucifer-donghua-DP-192x192.webp"
 }
 
-cloudstream {
-    setParams {
-        description = "Watch Donghua from Multi-Source"
-        authors = listOf("AC321-beep")
-        tvTypes = listOf(TvType.Anime) // Must use TvType enum
-        language = "zh"
-        version = 3
-        iconUrl = "https://i0.wp.com/luciferdonghua.in/wp-content/uploads/2022/12/cropped-lucifer-donghua-DP-192x192.webp"
-    }
+dependencies {
+    // These dependencies are required to resolve 'kotlinx', 'coroutineScope', and 'TvType'
+    compileOnly("com.lagradost:cloudstream3:pre-release")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 }
