edcc0e201751bd6e20fa610ee481bdc66ff93a17
AC321-beep <charanac4@gmail.com>
2026-06-29 10:40:49 +0530

    Create build.gradle.kts

---
+++ LuciferDonghua/build.gradle.kts
@@ -0,0 +1,9 @@
+cloudstream {
+    description = "Watch Donghua from Multi-Source"
+    authors = listOf("AC321-beep")
+    status = 1
+    tvTypes = listOf("Anime")
+    language = "zh"
+    version = 1
+    iconUrl = "https://donghuastream.org/wp-content/uploads/2023/05/Donghua-Stream--1024x1024.jpeg"
+}
