056905d8b36e63ffacd96c26ab6beaf3139a9a8c
AC321-beep <charanac4@gmail.com>
2026-06-29 10:26:33 +0530

    Create Extractors.kt

---
+++ LuciferDonghua/src/main/kotlin/com/luciferdonghua/Extractors.kt
@@ -0,0 +1,25 @@
+package com.luciferdonghua
+
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.SubtitleFile
+
+// You will add your specific video host extractors in this file later.
+// Example boilerplate below:
+
+/*
+open class ExampleHost : ExtractorApi() {
+    override val name = "ExampleHost"
+    override val mainUrl = "https://examplehost.com"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        // Extraction logic goes here
+    }
+}
+*/
