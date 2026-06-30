7c04c86ace85f89539d576016903c98ba8c5324f
AC321-beep <charanac4@gmail.com>
2026-06-30 12:33:39 +0530

    Update LuciferDonghuaPlugin.kt

--- LuciferDonghua/src/main/kotlin/com/luciferdonghua/LuciferDonghuaPlugin.kt
+++ LuciferDonghua/src/main/kotlin/com/luciferdonghua/LuciferDonghuaPlugin.kt
@@ -2,16 +2,15 @@ package com.luciferdonghua
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
 @CloudstreamPlugin
-class LuciferDonghuaPlugin : Plugin() {
-    override fun load() {
-        // 1. Register the Main Scraper
+class LuciferDonghuaPlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(LuciferDonghuaProvider())
         
-        // 2. Register all Extractors so loadExtractor() can use them
+        // ONLY Rumble remains registered here based on your Extractor.kt
         registerExtractorAPI(Rumble())
-        registerExtractorAPI(VidHideCustom())
-        registerExtractorAPI(VidHideProCustom())
     }
 }
