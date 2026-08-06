fa1d5ed5b7ecf6f9730db5b9750c5e9a8543a13d
AC321-beep <charanac4@gmail.com>
2026-08-06 15:29:12 +0530

    Create AnimekhorPlugin.kt

---
+++ Animekhor/src/main/kotlin/com/animekhor/AnimekhorPlugin.kt
@@ -0,0 +1,13 @@
+package com.Animekhor
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class AnimekhorPlugin : Plugin() {
+    override fun load(context: Context) {
+        // Register the main provider scraping logic
+        registerMainAPI(AnimekhorProvider())
+    }
+}
