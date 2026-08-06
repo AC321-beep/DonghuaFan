6e2e06603060dc2d8f6c8940e07975a49ae44ecd
AC321-beep <charanac4@gmail.com>
2026-08-06 15:32:50 +0530

    Create build.gradle.kts

---
+++ Animekhor/build.gradle.kts
@@ -0,0 +1,9 @@
+cloudstream {
+   description = "Watch Donghua & Anime streams from Animekhor"
+   authors = listOf("AC321-beep")
+   status = 1
+   tvTypes = listOf("Anime", "Movie")
+   language = "zh"
+   version = 1
+   iconUrl = "https://animekhor.org/wp-content/uploads/2021/04/Animekhor-Logo.png"
+}
