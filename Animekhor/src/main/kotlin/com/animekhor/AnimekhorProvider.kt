c1df914af62cb06ffd9d653c6672389de985950c
AC321-beep <charanac4@gmail.com>
2026-08-06 15:29:41 +0530

    Create AnimekhorProvider.kt

---
+++ Animekhor/src/main/kotlin/com/animekhor/AnimekhorProvider.kt
@@ -0,0 +1,134 @@
+package com.Animekhor
+
+import android.util.Log
+import android.util.Base64
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.loadExtractor
+import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.ParCollections.amap
+
+class AnimekhorProvider : MainAPI() {
+    override var mainUrl = "https://animekhor.org"
+    override var name = "Animekhor"
+    override val hasMainPage = true
+    override var lang = "zh"
+    override val hasDownloadSupport = true
+    override val supportedTypes = setOf(TvType.Movie, TvType.Anime)
+
+    override val mainPage = mainPageOf(
+        "anime/?status=ongoing&type=&order=update" to "Recently Updated",
+        "anime/?type=comic&order=update" to "Comic Recently Updated",
+        "anime/?type=comic" to "Comic Series",
+        "anime/?status=&type=ona&sub=&order=update" to "Donghua Recently Updated",
+        "anime/?status=&type=ona" to "Donghua Series",
+        "anime/?status=&sub=&order=latest" to "Latest Added",
+        "anime/?status=&type=&order=popular" to "Popular",
+        "anime/?status=completed&order=update" to "Completed"
+    )
+
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+        val document = app.get("$mainUrl/${request.data}&page=$page").document
+        val home = document.select("div.listupd > article").mapNotNull {
+            it.toSearchResult()
+        }
+        return newHomePageResponse(request.name, home)
+    }
+
+    private fun Element.toSearchResult(): SearchResponse? {
+        val title = this.select("div.bsx > a").attr("title")
+        val href = fixUrl(this.select("div.bsx > a").attr("href"))
+        val posterUrl = fixUrlNull(this.selectFirst("div.bsx > a img")?.getsrcAttribute())
+        
+        return newMovieSearchResponse(title, href, TvType.Movie) {
+            this.posterUrl = posterUrl
+        }
+    }
+
+    private fun Element.getsrcAttribute(): String {
+        val src = this.attr("src")
+        val dataSrc = this.attr("data-src")
+        return if (src.startsWith("http")) src else if (dataSrc.startsWith("http")) dataSrc else ""
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val searchResponse = mutableListOf<SearchResponse>()
+        for (i in 1..3) {
+            val document = app.get("$mainUrl/page/$i/?s=$query").document
+            val results = document.select("div.listupd > article").mapNotNull {
+                it.toSearchResult()
+            }
+            searchResponse.addAll(results)
+            if (results.isEmpty()) break
+        }
+        return searchResponse
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+        val document = app.get(url).document
+        val title = document.selectFirst("h1.entry-title")?.text()?.trim() ?: ""
+        var poster = document.select("meta[property=og:image]").attr("content")
+        val description = document.selectFirst("div.entry-content")?.text()?.trim()
+        val type = document.selectFirst(".spe")?.text()
+        
+        val tvtag = if (type?.contains("Movie", ignoreCase = true) == true) TvType.Movie else TvType.TvSeries
+
+        if (tvtag == TvType.Movie) {
+            if (poster.isEmpty()) {
+                poster = document.selectFirst("meta[property=og:image]")?.attr("content")?.trim() ?: ""
+            }
+            val href = document.selectFirst(".eplister li > a")?.attr("href") ?: ""
+            return newMovieLoadResponse(title, url, TvType.Movie, href) {
+                this.posterUrl = poster
+                this.plot = description
+            }
+        } else {
+            val epPage = document.selectFirst(".eplister li > a")?.attr("href") ?: ""
+            val doc = app.get(epPage).document
+            val epPoster = doc.select("meta[property=og:image]").attr("content")
+            
+            val episodes = doc.select("div.episodelist > ul > li").map { info ->
+                val href1 = info.select("a").attr("href")
+                // Parses episode numbering between dashes 
+                val episode = info.select("a span").text().substringAfter("-").substringBeforeLast("-").trim()
+                newEpisode(href1) {
+                    this.name = episode
+                    this.posterUrl = epPoster
+                }
+            }.reversed()
+
+            return newTvSeriesLoadResponse(title, url, TvType.Anime, episodes) {
+                this.posterUrl = poster
+                this.plot = description
+            }
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val document = app.get(data).document
+        val servers = document.select(".mobius option")
+        
+        servers.amap { server ->
+            val base64 = server.attr("value")
+            val decodedUrl = String(Base64.decode(base64, Base64.DEFAULT))
+            val regex = Regex("""src=["']([^"']+)["']""", RegexOption.IGNORE_CASE)
+            var url = regex.find(decodedUrl)?.groups?.get(1)?.value ?: "Not found"
+            
+            if (url.startsWith("//")) {
+                url = "https:$url"
+            }
+            
+            Log.d("Phisher", url)
+            loadExtractor(url, mainUrl, subtitleCallback, callback)
+        }
+        return true
+    }
+}
