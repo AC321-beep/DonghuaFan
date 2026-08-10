5618b3dc139d362e32351ca77760b208a9b0d91f
AC321-beep <charanac4@gmail.com>
2026-08-10 15:32:21 +0530

    Update AnimekhorPlugin.kt

--- Animekhor/src/main/kotlin/com/animekhor/AnimekhorPlugin.kt
+++ Animekhor/src/main/kotlin/com/animekhor/AnimekhorPlugin.kt
@@ -9,5 +9,15 @@ class AnimekhorPlugin : Plugin() {
     override fun load(context: Context) {
         // Register the main provider scraping logic
         registerMainAPI(AnimekhorProvider())
+        
+        // Explicitly register all custom extractors to override the broken generic ones
+        registerExtractorAPI(Embedwish())
+        registerExtractorAPI(Filelions())
+        registerExtractorAPI(P2pstream())
+        registerExtractorAPI(UpnsLive())
+        registerExtractorAPI(Swhoi())
+        registerExtractorAPI(VidHidePro5())
+        registerExtractorAPI(Emturbovid())
+        registerExtractorAPI(Rumble())
     }
 }
