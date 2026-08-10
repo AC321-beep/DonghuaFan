c0ff8eec7f5d3ca049782af1becad84633e2a4ab
AC321-beep <charanac4@gmail.com>
2026-08-10 15:34:25 +0530

    Update build.gradle.kts

--- Animekhor/build.gradle.kts
+++ Animekhor/build.gradle.kts
@@ -4,6 +4,6 @@ cloudstream {
    status = 1
    tvTypes = listOf("Anime", "Movie")
    language = "zh"
-   version = 3
+   version = 4
    iconUrl = "https://animekhor.org/wp-content/uploads/2021/04/Animekhor-Logo.png"
 }
