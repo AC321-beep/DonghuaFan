fb51e49c15544b4cb5a135565ef30337b6410778
AC321-beep <charanac4@gmail.com>
2026-08-11 14:35:02 +0530

    Update AnimekhorPlugin.kt

--- Animekhor/src/main/kotlin/com/animekhor/AnimekhorPlugin.kt
+++ Animekhor/src/main/kotlin/com/animekhor/AnimekhorPlugin.kt
@@ -2,23 +2,10 @@ package com.animekhor
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
 class AnimekhorPlugin : Plugin() {
-    override fun load(context: Context) {
+    override fun load() {
         registerMainAPI(AnimekhorProvider())
-        
-        // Explicitly register ALL dropdown sources
-        registerExtractorAPI(Embedwish())
-        registerExtractorAPI(Filelions())
-        registerExtractorAPI(Swhoi())
-        registerExtractorAPI(VidHidePro5())
-        registerExtractorAPI(P2pstream())
-        registerExtractorAPI(UpnsLive())
-        registerExtractorAPI(Emturbovid())
-        registerExtractorAPI(Listeamed())
-        registerExtractorAPI(AbyssPlayer())
-        registerExtractorAPI(Rumble())
     }
 }
