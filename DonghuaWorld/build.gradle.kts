bfdf28c9f99949e1327382263fb1e1a3afeeea3c
AC321-beep <charanac4@gmail.com>
2026-08-15 20:02:33 +0530

    Create build.gradle.kts

---
+++ DonghuaWorld/build.gradle.kts
@@ -0,0 +1,9 @@
+cloudstream {
+    description = "Watch Donghua & Anime streams from DonghuaWorld"
+    authors = listOf("AC321-beep")
+    status = 1                     // 1 = Working
+    tvTypes = listOf("Anime", "Movie", "Comic")
+    language = "zh"
+    version = 1
+    iconUrl = "https://www.google.com/s2/favicons?domain=donghuaworld.com&sz=256"
+}
