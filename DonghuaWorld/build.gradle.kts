fac2f170fb42d59ef4d5682c5980727bd043aaeb
AC321-beep <charanac4@gmail.com>
2026-08-15 21:42:17 +0530

    Update build.gradle.kts

--- DonghuaWorld/build.gradle.kts
+++ DonghuaWorld/build.gradle.kts
@@ -4,6 +4,6 @@ cloudstream {
     status = 1                     // 1 = Working
     tvTypes = listOf("Anime", "Movie", "Comic")
     language = "zh"
-    version = 1
+    version = 2
     iconUrl = "https://www.google.com/s2/favicons?domain=donghuaworld.com&sz=256"
 }
