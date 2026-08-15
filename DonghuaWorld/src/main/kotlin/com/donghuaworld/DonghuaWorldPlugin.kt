652a93c9ed553fa4a81765e5c62b7016007582c7
AC321-beep <charanac4@gmail.com>
2026-08-15 19:59:13 +0530

    Update DonghuaWorldPlugin.kt

--- DonghuaWorld/src/main/kotlin/com/donghuaworld/DonghuaWorldPlugin.kt
+++ DonghuaWorld/src/main/kotlin/com/donghuaworld/DonghuaWorldPlugin.kt
@@ -1,1 +1,11 @@
+package com.donghuaworld
 
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DonghuaWorldPlugin : Plugin() {
+    override fun load() {
+        registerMainAPI(DonghuaWorldProvider())
+    }
+}
