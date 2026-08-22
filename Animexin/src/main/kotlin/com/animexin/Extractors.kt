6395be1d7bbb7b2b400a1015a1c2bafdeff12441
AC321-beep <charanac4@gmail.com>
2026-08-22 11:09:19 +0530

    Create Extractors.kt

---
+++ Animexin/src/main/kotlin/com/animexin/Extractors.kt
@@ -0,0 +1,54 @@
+package com.Animexin
+
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.extractors.Filesim
+import com.lagradost.cloudstream3.extractors.StreamSB
+import com.lagradost.cloudstream3.extractors.StreamWishExtractor
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
+import com.lagradost.cloudstream3.utils.JsUnpacker
+import com.lagradost.cloudstream3.utils.Qualities
+
+class FileMoonSx : Filesim() {
+    override val name = "FileMoonSx"
+    override val mainUrl = "https://filemoon.sx"
+}
+
+class Waaw : StreamSB() {
+    override var mainUrl = "https://waaw.to"
+}
+
+class Wishfast : StreamWishExtractor() {
+    override var name = "StreamWish"
+    override var mainUrl = "https://wishfast.top"
+}
+
+class Vtbe : ExtractorApi() {
+    override val name = "Vtbe"
+    override val mainUrl = "https://vtbe.to"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
+        val response = app.get(url, referer = mainUrl).document
+        
+        // Find the packed Javascript script tag
+        val script = response.selectFirst("script:containsData(function(p,a,c,k,e,d))")?.data() ?: return null
+        
+        // Unpack and extract the .m3u8 file
+        val unpacked = JsUnpacker(script).unpack() ?: return null
+        val match = Regex("""sources:\[\{file:"(.*?)"""").find(unpacked)
+        val link = match?.groupValues?.get(1) ?: return null
+
+        return listOf(
+            ExtractorLink(
+                source = name,
+                name = name,
+                url = link,
+                referer = referer ?: "",
+                quality = Qualities.Unknown.value,
+                type = ExtractorLinkType.M3U8
+            )
+        )
+    }
+}
