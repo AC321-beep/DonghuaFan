1fd8744a2a990aa302483eb2767c808c5be79598
AC321-beep <charanac4@gmail.com>
2026-08-22 11:17:40 +0530

    Create build.gradle.kts

---
+++ Animexin/build.gradle.kts
@@ -0,0 +1,9 @@
+cloudstream {
+   description = "Watch Donghua & Anime streams from Animexin"
+   authors = listOf("AC321-beep")
+   status = 1
+   tvTypes = listOf("Anime", "Movie")
+   language = "zh"
+   version = 1
+   iconUrl = "https://www.google.com/s2/favicons?domain=animexin.dev&sz=256"
+}
