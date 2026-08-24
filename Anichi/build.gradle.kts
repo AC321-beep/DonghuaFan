c5102fea4dd174c80a8a1b05ba7078dc95a049b7
AC321-beep <charanac4@gmail.com>
2026-08-24 14:47:52 +0530

    Create build.gradle.kts

---
+++ Anichi/build.gradle.kts
@@ -0,0 +1,9 @@
+cloudstream {
+   description = "Watch Anime streams from Anichi"
+   authors = listOf("AC321-beep")
+   status = 1
+   tvTypes = listOf("Anime", "AnimeMovie")
+   language = "en"
+   version = 1
+   iconUrl = "https://www.google.com/s2/favicons?domain=allanimenews.com&sz=256"
+}
