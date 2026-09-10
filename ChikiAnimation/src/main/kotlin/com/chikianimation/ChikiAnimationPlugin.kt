289c8649a1852ed0dbc5731fbbb54cc2aaf30fde
AC321-beep <charanac4@gmail.com>
2026-09-10 16:26:27 +0530

    Update ChikiAnimationPlugin.kt

--- ChikiAnimation/src/main/kotlin/com/chikianimation/ChikiAnimationPlugin.kt
+++ ChikiAnimation/src/main/kotlin/com/chikianimation/ChikiAnimationPlugin.kt
@@ -1,7 +1,9 @@
 package com.chikianimation
 
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
+@CloudstreamPlugin
 class ChikiAnimationPlugin : Plugin() {
     override fun load() {
         registerMainAPI(ChikiAnimationProvider())
