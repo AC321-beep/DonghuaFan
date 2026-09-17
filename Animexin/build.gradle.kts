2b115c3114a8550e115d09c8d5c947215b6bf03e
AC321-beep <charanac4@gmail.com>
2026-09-17 14:42:25 +0530

    Update build.gradle.kts

--- Animexin/build.gradle.kts
+++ Animexin/build.gradle.kts
@@ -1,9 +1,9 @@
 cloudstream {
-   description = "Watch Donghua & Anime streams from Animexin"
-   authors = listOf("AC321-beep")
-   status = 1
-   tvTypes = listOf("Anime", "Movie")
-   language = "zh"
-   version = 6
-   iconUrl = "https://animexin.dev/wp-content/uploads/2026/01/cropped-New-Logo-e1768365053967-192x192.png"
+    description = "Watch Donghua & Anime streams from Animexin"
+    authors = listOf("AC321-beep")
+    status = 1
+    tvTypes = listOf("Anime", "Movie")
+    language = "zh"
+    version = 6
+    iconUrl = "https://www.google.com/s2/favicons?sz=128&domain=animexin.dev"
 }
