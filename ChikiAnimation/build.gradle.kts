5b1a15821f16ebdfa6b4d176a78d89c66b0493be
AC321-beep <charanac4@gmail.com>
2026-09-24 16:29:40 +0530

    Update build.gradle.kts

--- ChikiAnimation/build.gradle.kts
+++ ChikiAnimation/build.gradle.kts
@@ -4,6 +4,6 @@ cloudstream {
    status = 1
    tvTypes = listOf("Anime", "Movie")
    language = "zh"
-   version = 5
+   version = 6
    iconUrl = "https://www.google.com/s2/favicons?domain=chikianimation.com&sz=256"
 }
