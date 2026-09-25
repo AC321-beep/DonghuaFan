f0fe283528a0c0bb7ddbaf1d9da7afa9a36b9323
AC321-beep <charanac4@gmail.com>
2026-09-25 22:05:44 +0530

    Update build.gradle.kts

--- Adfree/build.gradle.kts
+++ Adfree/build.gradle.kts
@@ -7,7 +7,7 @@ cloudstream {
     authors = listOf("AC321-beep")   
     status = 1
     language = "en"
-    version = 7
+    version = 8
     iconUrl = "https://raw.githubusercontent.com/AC321-beep/DonghuaFan/refs/heads/master/Adfree/Icon.png"
 }
 
